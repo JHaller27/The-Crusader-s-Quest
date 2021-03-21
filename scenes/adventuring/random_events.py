eaf82982f23a3de281dc422ba3ae56a7fed9f30c
James Haller <jameshaller27@gmail.com>
2021-03-21 00:33:27 -0700

    Replace Mushroom in random_events

--- scenes/adventuring/random_events.py
+++ scenes/adventuring/random_events.py
@@ -8,7 +8,6 @@ import scenes.adventuring.traveller as traveller
 import scenes.adventuring.fight as fight
 import scenes.adventuring.adventuring as adventuring
 import scenes.adventuring.robbed as robbed
-import scenes.adventuring.mushroom as mushroom
 
 ui = Singleton()
 
@@ -23,6 +22,44 @@ class RandomEvent(State):
         next_state: State = random.choices(STATE_CHOICES, WEIGHT_CHOICES)[0](ctx)
 
         return next_state
+
+
+class Mushroom(State):
+    def do(self) -> Optional[State]:
+        ctx = self.ctx
+
+        ui.print("You see a strange mushroom.")
+        selection = ui.choose(["Consume", "Leave"])
+        if selection == 2:
+            ui.print("You leave the mushroom.")
+
+        elif ctx.player.is_race("Satyr"):
+            extra_endurance = 3 * ctx.player.consumption_rate
+            ctx.player.endurance += extra_endurance
+            ui.print(f"You eat the mushroom, and gain {extra_endurance} Endurance.")
+
+        else:
+            event = random.randint(1, 4)
+
+            if event == 1:
+                ui.print("You eat the mushroom, and nothing happens.")
+
+            elif event == 2:
+                ui.print("You eat the mushroom, and it causes you to vomit.")
+
+            elif event == 3:
+                extra_endurance = 2 * ctx.player.consumption_rate
+                ctx.player.consumption_rate += extra_endurance
+                ui.print(f"You eat the mushroom, and gain {extra_endurance} Endurance.")
+
+            else:
+                ctx.player.hp = 0
+                ctx.endurance = 0
+
+                ui.print("You eat the mushroom, and then fall to the ground, foaming at the mouth.")
+
+        ui.wait()
+        return adventuring.Adventuring(self.ctx)
 
 
 # Miracle #
@@ -262,7 +299,7 @@ EVENT_MAP = [
     (traveller.Traveller, 2),
     (Damaged, 2),
     (Miracle, 1),
-    (mushroom.Mushroom, 1),
+    (Mushroom, 1),
     (Mystic, 1),
     (BiggerBag, 1),
     (LoseDay, 1),
